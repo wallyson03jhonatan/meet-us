a29c21bf97e860ac9adb3264331fa4736378415f
wallyson03jhonatan <wallyson.oliveira@futurasistemas.com.br>
2024-10-08 11:58:12 -0300

    fix: simplify text on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="p-4 flex flex-col items-center justify-center bg-gray-100 dark:bg-gray-800">
-    <span class="text-sm font-mono">Versão v1.0.0 XXXX-XX-XX</span>
+    <span class="text-sm font-mono">Meet-us @2024</span>
     <span class="text-sm font-mono">Todos os direitos reservados.</span>
   </footer>
 </template>
