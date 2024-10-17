e87a569b96b7282aaa7d737064797cc81c8d2ebf
Wallyson <wallyson03jhonatan@gmail.com>
2024-10-16 21:43:21 -0300

    fix: fix about columns

--- components/Section/About/index.vue
+++ components/Section/About/index.vue
@@ -6,7 +6,7 @@
       <p class="mt-1 font-normal">{{ $t("sobreDescricao") }}</p>
     </div>
 
-    <div class="my-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+    <div class="my-16 grid grid-cols-1 lg:grid-cols-3 gap-16">
 
       <template v-if="loading">
         <USkeleton class="min-h-[300px] min-w-full" />
